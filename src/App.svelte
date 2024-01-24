77917ccdc4c6c105d4d6e06e9e07c7ee1399a0f1
zzmin0405 <116485459+zzmin0405@users.noreply.github.com>
2024-01-24 23:32:35 +0900

    svelte-learning

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let name = 'MAX';
+	let age = 30
+	function incrementage(){
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: tomato;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}</h1>
+<button on:click={incrementage}>Change age</button>
