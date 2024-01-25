2ef07898db7be1f533a4e240609d40f522593bb5
zzmin0405 <wlals12363@gmail.com>
2024-01-25 15:49:05 +0900

    project-set

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,36 @@
 <script>
-	let name = 'MAX';
-	let age = 30
-	function incrementage(){
-		age += 1;
-	}
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
 </script>
 
 <style>
-	h1 {
-		color: tomato;
-	}
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
 </style>
 
-<h1>Hello {name}, my age is {age}</h1>
-<button on:click={incrementage}>Change age</button>
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
